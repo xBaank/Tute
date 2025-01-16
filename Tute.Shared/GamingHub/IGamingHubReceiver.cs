eb42acc4398b975f7fe1532dda6322a718ba45e3
Bank <anom@gmail.com>
2025-01-16 13:43:44 +0100

    WIP

--- Tute.Shared/GamingHub/IGamingHubReceiver.cs
+++ Tute.Shared/GamingHub/IGamingHubReceiver.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using Tute.Shared.Models;
+﻿using Tute.Shared.Models;
 
 namespace Tute.Shared.GamingHub
 {
     public interface IGamingHubReceiver
     {
-        void OnJoin(Guid uuid);
-        void OnLeave(Guid uuid);
+        void OnJoin(Player uuid);
+        void OnLeave(Player uuid);
         void OnMove(CardData card);
     }
 }
