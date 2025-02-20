320a2994dc12b0d754fd45b6fe1c13946d0a373c
Bank <bank@example.com>
2025-02-20 10:11:13 +0100

    Cleanup

--- Tute.Server/Controllers/ChatController.cs
+++ Tute.Server/Controllers/ChatController.cs
@@ -6,14 +6,12 @@ namespace Tute.Server.Controllers;
 
 internal class ChatController(IGroup<IGamingHubReceiver> room, Player self)
 {
-    private readonly List<(Player sender, string message)> messages = [];
 
     public ValueTask SendMessage(string message)
     {
         var max = Math.Min(message.Length, 100);
         var trimmed = message.Trim()[..max];
         if (string.IsNullOrWhiteSpace(trimmed)) return ValueTask.CompletedTask;
-        messages.Add((self, trimmed));
         room.All.OnMessage(trimmed, self);
         return ValueTask.CompletedTask;
     }
